472a8247db9280db474548011b2593cb6954ebc4
Sonsawan Ngamsom <86834525+murphy6867@users.noreply.github.com>
2023-10-06 11:50:12 +0700

    Make Sign Up UI

--- Frontend/src/pages/SignUp.jsx
+++ Frontend/src/pages/SignUp.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import Link from 'react-router-dom/Link';
+
 
 export default function SignUp() {
   return (
-    <div>SignOut</div>
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <form action="" className="flex flex-col gap-4">
+        <input type="text" placeholder="User name ..." 
+          className="border p-3 rounded-lg" id="username" />
+        <input type="text" placeholder="User email ..." 
+          className="border p-3 rounded-lg" id="email" />
+        <input type="text" placeholder="User password ..." 
+          className="border p-3 rounded-lg" id="password" />
+        <button 
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disable:opacity-80">
+          Sign up
+        </button>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={'/sign-in'}>
+          <span className='text-blue-700'>Sign In</span>
+        </Link>
+      </div>
+    </div>
   )
 }
